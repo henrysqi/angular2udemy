341410bea74128dc69a4cff4e5150c5cd3430c85
Henry Qi <henryqi222@gmail.com>
2016-12-17 19:48:02 -0700

    30

---
+++ first-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>Root Component</h1>
+    <fa-lifecycle></fa-lifecycle>
+  `,
+  
+})
+export class AppComponent {
+
+}
---
+++ first-app/src/app/databinding/event-binding.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'fa-event-binding',
+  template: `
+    <button (click)="onClicked()">Click Me</button>
+  `,
+  styles: []
+})
+export class EventBindingComponent {
+  @Output('clickable') clicked = new EventEmitter<string>();
+  onClicked = function(){
+    this.clicked.emit("wtf is going on")
+  }
+}
