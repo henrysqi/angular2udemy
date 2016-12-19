51306543cf51cd7b1c0080424a24915a21acd64e
Henry Qi <henryqi222@gmail.com>
2016-12-18 22:18:26 -0700

    52 before serve

--- recipe-book/src/app/recipes/recipe.ts
+++ recipe-book/src/app/recipes/recipe.ts
@@ -1,5 +1,5 @@
+import { Ingredient } from "../ingredient";
+
 export class Recipe {
-    constructor(public name, public description, public imagePath){
-        
-    }
+  constructor(public name, public description, public imagePath, public ingredients: Ingredient[]) {}
 }
